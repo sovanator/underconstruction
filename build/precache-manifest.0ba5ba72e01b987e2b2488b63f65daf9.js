93303d5bae592c40e61fa0d2850dc24062012ad9
sovanator <40123873+sovanator@users.noreply.github.com>
2020-01-30 09:46:01 -0800

    Add files via upload

---
+++ build/precache-manifest.0ba5ba72e01b987e2b2488b63f65daf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42e52f0b70c0dd26ba8ec578b888dfee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51d8abf13bd8dda50a72",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "17ef1b1f53f2c5a191ea",
+    "url": "/static/js/2.d0d14602.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d0d14602.chunk.js.LICENSE"
+  },
+  {
+    "revision": "51d8abf13bd8dda50a72",
+    "url": "/static/js/main.713196bd.chunk.js"
+  },
+  {
+    "revision": "2a969376a32de981bedc",
+    "url": "/static/js/runtime-main.e214b6fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
