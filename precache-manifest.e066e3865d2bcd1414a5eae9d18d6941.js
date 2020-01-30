a81f92341523e741527f737fcdbd1fdcc729a741
sovanator <sunham.kc@gmail.com>
2020-01-30 09:35:37 -0800

    first commit

---
+++ precache-manifest.e066e3865d2bcd1414a5eae9d18d6941.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2da12498a662ae871518e8a273b8e25",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad6d7352713297e9af6d",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "17ef1b1f53f2c5a191ea",
+    "url": "/static/js/2.d0d14602.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d0d14602.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ad6d7352713297e9af6d",
+    "url": "/static/js/main.9f4c9ba6.chunk.js"
+  },
+  {
+    "revision": "2a969376a32de981bedc",
+    "url": "/static/js/runtime-main.e214b6fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
